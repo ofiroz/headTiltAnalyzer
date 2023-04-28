2c95b7b0795c127d9453f35f414f963e65f1ee4f
Ofir Ozeri <ofir.ozeri@stargo.co>
2023-04-28 19:24:55 +0300

    first task logic - done
    todo: refine flow design

--- flow_managment/flow_managers/first_task_flow_manager.py
+++ flow_managment/flow_managers/first_task_flow_manager.py
@@ -1,4 +1,4 @@
-from configuration.config import EYE_DETECTION_CASCADE_PATH, DETECTION_RECORDS_PATH
+from configuration.config import EYE_DETECTION_CASCADE_PATH, DETECTION_RECORDS_PATH, SAVE_FRAMES, CAMERA_PORT
 from IO_operations.eyes_detection_record import append_detections_to_text_file
 from flow_managment.task_flow_manager import TaskFlowManager
 from generic.thread_helper import create_new_thread
@@ -11,28 +11,25 @@ class FirstTaskFlowManager(TaskFlowManager):
 
     def __init__(self):
         self.eye_cascade = cv2.CascadeClassifier(EYE_DETECTION_CASCADE_PATH)
-        self.default_camera_port = 1  # todo: change to 0
+        self.default_camera_port = CAMERA_PORT
         self.capture = cv2.VideoCapture(self.default_camera_port)
         self.transparency = 0.2
+        self.frame_list: list[np.ndarray] = []
 
     def _get_eye_box_list(self, frame, scaleFactor=1.3, minNeighbors=5):
         # The following method returns the coordinates for every eye detected
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         return self.eye_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
 
-    def _brighten_eye_box(self, frame):
-        # TODO
-        return frame
+    def _brighten_detected_box(self, det_list, frame):
+        overlay = frame.copy()
+        for (x, y, w, h) in det_list:
+            cv2.rectangle(overlay, (x, y), (x + w, y + h), (255, 255, 255), -1)
+        return overlay
 
-    # @staticmethod
-    # def test(a):
-    #     while True:
-    #         print(f'a: {str(a)}')
-    #         a += 1
-    #         time.sleep(1)
-
-    def _record_frame(self, frame):
-        append_detections_to_text_file(r'C:\Users\ofir\Downloads\ttt.txt', frame)
+    @staticmethod
+    def _detected(eyes):
+        return len(eyes)
 
     def _analyze_live_feed(self):
         """
@@ -40,22 +37,21 @@ class FirstTaskFlowManager(TaskFlowManager):
         The eyes in each frame will be brighten and every detection will be recorded using append_detections_to_text_file function
 
         * press 'q' to continue.
+        ** FYI, SAVE_FRAME flag is True by default todo
         """
         while True:
             _, frame = self.capture.read()
-            overlay = frame.copy()
             eyes = self._get_eye_box_list(frame)
-            if isinstance(eyes, np.ndarray) and len(eyes) == 0:
-                p=0 # TODO
-            for (x, y, w, h) in eyes:
-                cv2.rectangle(overlay, (x, y), (x+w, y+h), (255, 255, 255), -1)
+            if SAVE_FRAMES and self._detected(eyes):
+                self.frame_list.append(frame)
+            overlay = self._brighten_detected_box(eyes, frame)
             frame = cv2.addWeighted(overlay, self.transparency, frame, 1 - self.transparency, 0)
             cv2.imshow('LiveVideo', frame)
 
-            # record the frame in cases of detection
-            # create_new_thread(self.test, a=2)
-
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                if SAVE_FRAMES:
+                    # append_detections_to_text_file(r'C:\Users\ofir\Downloads\ttt.txt', self.frame_list)
+                    create_new_thread(append_detections_to_text_file, filepath=r'C:\Users\ofir\Downloads\ttt.txt', single_frame_dets=self.frame_list)
                 break
 
     def flow(self):
@@ -68,4 +64,4 @@ class FirstTaskFlowManager(TaskFlowManager):
         self.capture.release()
         cv2.destroyAllWindows()
 
-FirstTaskFlowManager().flow()
+# FirstTaskFlowManager().flow()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flow_managment.main_flow_manager import MainFlowManager
 from configuration import config
 
+# todo
+"""
+mention the need for a production ready and scalable project in the readme 
+"""
+
 
 def main():
     pass
